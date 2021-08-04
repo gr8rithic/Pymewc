e87a573a0a70381d9bce1324080f451f9c7b5bbc
Rithic Hariharan <60564768+gr8rithic@users.noreply.github.com>
2021-08-04 12:53:04 +0530

    Add files via upload

--- build/lib/pymewc/__init__.py
+++ build/lib/pymewc/__init__.py
@@ -1,29 +1,27 @@
 from os import error
+import time
+
+print("Thanks for using pymewc to record any issue mail to gr8rithic@gmail.com")
+time.sleep(1)
+print('----------------------------------------------------------------------------------------------------')
+time.sleep(1)
+print("You can also get in touch with me on github link https://github.com/gr8rithic/")
+time.sleep(1)
+print('----------------------------------------------------------------------------------------------------')
+time.sleep(1)
+print('Get connected via LinkedIn https://www.linkedin.com/in/rithic-hariharan-8902b4199/')
+time.sleep(1)
+print('----------------------------------------------------------------------------------------------------')
+time.sleep(1)
+print('Visit my postfolio https://gr8rithic.github.io/')
+time.sleep(1)
+print('----------------------------------------------------------------------------------------------------')
+time.sleep(1)
+print()
+print("Setting up the environment")
+time.sleep(3)
 
 def serial():
-    import time
-
-    print("Thanks for using pymewc to record any issue mail to gr8rithic@gmail.com")
-    time.sleep(1)
-    print('----------------------------------------------------------------------------------------------------')
-    time.sleep(1)
-    print("You can also get in touch with me on github link https://github.com/gr8rithic/")
-    time.sleep(1)
-    print('----------------------------------------------------------------------------------------------------')
-    time.sleep(1)
-    print('Get connected via LinkedIn https://www.linkedin.com/in/rithic-hariharan-8902b4199/')
-    time.sleep(1)
-    print('----------------------------------------------------------------------------------------------------')
-    time.sleep(1)
-    print('Visit my postfolio https://gr8rithic.github.io/')
-    time.sleep(1)
-    print('----------------------------------------------------------------------------------------------------')
-    time.sleep(1)
-    print()
-    print("Setting up the environment")
-    time.sleep(3)
-
-
     com_port = input("Enter the com-port(Com4 or /dev/ttyACM0 on Windows and Unix based respectively)")
     baud_rate = int(input("Enter the baud rate as specified in the .ino file"))
     import serial  
@@ -34,7 +32,6 @@ def serial():
 
     while(1):
         try:
-            
             x = (ser.readline().strip())            
             print("Check the usb port is mentioned correctly or usb is connected properly")
             y = (x.decode('utf-8'))       
@@ -44,5 +41,5 @@ def serial():
             continue                      
 
 def hello():
-    print("Hellow world")
+    print("Hello world, The package is properly installed version = 0.0.7.4")
 
